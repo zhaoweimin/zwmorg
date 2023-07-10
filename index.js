00654ddd03cf1be06b9e28d38f27c5baac522a73
zwm <464073319@qq.com>
2023-07-10 14:32:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+export default () => {
+    console.log('这里是zwmorg')
+}
---
+++ packages/b/index.js
@@ -0,0 +1,6 @@
+import a from '@zwmorg/a'
+console.log('这里是zwmorg/b，引入了a并调用：')
+a()
+// export default () => {
+//     console.log('这里是zwmorg/b')
+// }
